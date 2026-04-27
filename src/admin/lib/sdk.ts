a8aa2214d879d3edbb5bef191607b46e3de409b0
Maciej <fajerowsky.yt@gmail.com>
2026-04-27 14:56:42 +0200

    fix: url issues

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const sdk = new Medusa({
-    baseUrl: import.meta.env.VITE_BACKEND_URL || "/",
+    baseUrl: __BACKEND_URL__,
     debug: import.meta.env.DEV,
     auth: {
         type: "session",
